af23af9e24005aa3ea4c7723a811b264f2fa58d3
Darko Janekovic <darko.janekovic@fer.hr>
2019-02-22 16:29:27 +0100

    Nedelec basis for 2D done
    This commit implements Nedelec basis functions. Data structures used in
    this commit are somewhat complicated and will be further explained.
    
    Data structures
    ==============
    
    - PetscScalar *fspace.val is 3D matrix which are just multiple 2D
    matrices. We store 3 different 2D matrices - one for each basis
    function. 3D case will have 6 different 2D matrices because of 6 basis
    functions in 3D case.
    
    - fspace.val    M x 2/3 x 3/4
    - fspace.cval   M x 1/3 x 3/4
    
    NOTE: this commit heavily relies on changes in quadrature.c. Quadrature
    rules will be commited in the next commit.
    
    Signed-off-by: Darko Janekovic <darko.janekovic@fer.hr>

---
+++ src/nedelec.c
@@ -0,0 +1,35 @@
+#include <petscmat.h>
+
+#include "quadrature.h"
+#include "nedelec.h"
+
+/**
+ * M represents number of quadrature points
+ */
+PetscErrorCode nedelec_basis(struct quadrature q, struct function_space *fspace) {
+    PetscInt M = q.size;
+
+    /* three different M x 2 matrix */
+    PetscErrorCode ierr = PetscMalloc1(3 * M * 2, &fspace->val); CHKERRQ(ierr);
+    ierr = PetscMalloc1(3 * M * 1, &fspace->cval); CHKERRQ(ierr);
+
+    //TODO: clever define for this ugly addressing
+    for (PetscInt i = 0; i < M; i++) {
+        fspace->val[0 * (M * 2) + i * M + 0] = -q.pw[i * 3 + 1];
+        fspace->val[0 * (M * 2) + i * M + 1] = q.pw[i * 3 + 0];
+
+        fspace->val[1 * (M * 2) + i * M + 0] = -q.pw[i * 3 + 1];
+        fspace->val[1 * (M * 2) + i * M + 1] = q.pw[i * 3 + 0] - 1;
+
+        fspace->val[2 * (M * 2) + i * M + 0] = 1 - q.pw[i * 3 + 1];
+        fspace->val[2 * (M * 2) + i * M + 1] = q.pw[i * 3 + 0];
+
+        fspace->cval[i * 3 + 0] = 2;
+        fspace->cval[i * 3 + 1] = 2;
+        fspace->cval[i * 3 + 2] = 2;
+    }
+
+    fspace->nbasis = 3;
+
+    return ierr;
+}
---
+++ src/nedelec.h
@@ -0,0 +1,13 @@
+#ifndef NEDELEC_H
+#define NEDELEC_H
+
+
+struct function_space {
+    PetscScalar *val;
+    PetscScalar *cval;
+    PetscInt nbasis;
+};
+
+PetscErrorCode nedelec_basis(struct quadrature q, struct function_space *fspace);
+
+#endif /* NEDELEC_H */
