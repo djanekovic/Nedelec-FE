8f20d3378774fe603d4208a563fcd04f723caf17
Darko Janekovic <darko.janekovic@fer.hr>
2019-03-24 09:39:51 +0100

    function: init for function abstraction
    These files will represent function abstraction. For now, only supported
    function is constant. Hopefully API won't change too much.
    
    Signed-off-by: Darko Janekovic <darko.janekovic@fer.hr>

---
+++ src/function.c
@@ -0,0 +1,12 @@
+#include <petsc.h>
+
+//TODO: inline this
+PetscScalar constant_2D(PetscReal x, PetscReal y)
+{
+    return 1.0;
+}
+
+PetscScalar constant_3D(PetscReal x, PetscReal y, PetscReal z)
+{
+    return 1.0;
+}
---
+++ src/function.h
@@ -0,0 +1,7 @@
+#ifndef FUNCTION_H
+#define FUNCTION_H
+
+PetscScalar constant_2D(PetscReal, PetscReal);
+PetscScalar constant_3D(PetscReal, PetscReal, PetscReal);
+
+#endif /* FUNCTION_H */
