d0044937af8500f6ef8033c3f351236d56a1fa40
Darko Janekovic <darko.janekovic@fer.hr>
2019-03-25 12:54:47 +0100

    mesh.c: Added debug print and bug fix
    Signs matrix was calculated wrong because of mistake in node
    orientation. Routine for generating signs matrix, now takes edge
    orientation into account.
    
    Discrete gradient matrix is maybe also buggy. Fix for that in the next
    commit.
    
    Signed-off-by: Darko Janekovic <darko.janekovic@fer.hr>

--- src/mesh.c
+++ src/mesh.c
@@ -5,6 +5,7 @@
 #include "util.h"
 
 static PetscErrorCode mark_boundary_faces(DM dm);
+static PetscErrorCode debug_print(DM dm);
 static PetscErrorCode free_ctx(void **data);
 
 /**
@@ -29,7 +30,7 @@ PetscErrorCode generate_mesh(struct ctx *sctx, DM *dm) {
      * MPI commutator - PETSC_COMM_WORLD;
      * dim - sctx->dim;
      * simplex - PETSC_TRUE (for tensor cell, PETSC_FALSE)
-     * faces - number of faces per dimension TODO
+     * faces - number of faces per dimension TODO add to solver ctx
      * lower left corner - (0, 0, 0) if NULL
      * upper_right_corner - (1, 1, 1) if NULL
      * periodicity - DM_BOUNDARY_NONE
@@ -40,6 +41,7 @@ PetscErrorCode generate_mesh(struct ctx *sctx, DM *dm) {
     CHKERRQ(ierr);
 
     ierr = mark_boundary_faces(*dm); CHKERRQ(ierr);
+    ierr = debug_print(*dm); CHKERRQ(ierr);
 
     DMPlexGetHeightStratum(*dm, 0, &cstart, &cend);
     DMPlexGetHeightStratum(*dm, 1, &estart, &eend);
@@ -68,15 +70,18 @@ PetscErrorCode generate_mesh(struct ctx *sctx, DM *dm) {
      * If node[0] > node[1] we set 1 in signs matrix, else we set zero.
      */
     for (PetscInt c = cstart; c < cend; c++) {
+        PetscInt offset = (c - cstart) * 3;
         const PetscInt *edgelist;
+        const PetscInt *orient;
         ierr = DMPlexGetCone(*dm, c, &edgelist); CHKERRQ(ierr);
+        ierr = DMPlexGetConeOrientation(*dm, c, &orient);
         for (PetscInt i = 0; i < edgenum; i++) {
             const PetscInt *nodes;
             DMPlexGetCone(*dm, edgelist[i], &nodes);
             if (nodes[0] < nodes[1]) {
-                sctx->signs[(c - cstart) * 3 + i] = 1;
+                sctx->signs[offset + i] = (orient[i] >= 0) ? 1 : -1;
             } else {
-                sctx->signs[(c - cstart) * 3 + i] = -1;
+                sctx->signs[offset + i] = (orient[i] >= 0) ? -1 : 1;
             }
             ierr = MatSetValue(sctx->G,
                                edgelist[i]-estart, nodes[0]-vstart,
@@ -122,4 +127,33 @@ static PetscErrorCode free_ctx(void **data)
     ierr = PetscFree(sctx->signs); CHKERRQ(ierr);
 
     return ierr;
+}
+
+static PetscErrorCode debug_print(DM dm)
+{
+    Vec c;
+    int cstart, cend, vstart, vend, estart, eend;
+
+    DMView(dm, PETSC_VIEWER_STDOUT_WORLD);
+    DMGetCoordinates(dm, &c);
+    VecView(c, PETSC_VIEWER_STDOUT_WORLD);
+
+    DMPlexGetHeightStratum(dm, 0, &cstart, &cend);
+    DMPlexGetHeightStratum(dm, 1, &estart, &eend);
+    DMPlexGetHeightStratum(dm, 2, &vstart, &vend);
+
+    for (int c = cstart; c < cend; c++) {
+        const PetscInt *edges;
+        const PetscInt *orient;
+        DMPlexGetCone(dm, c, &edges);
+        DMPlexGetConeOrientation(dm, c, &orient);
+        for (int i = 0; i < 3; i++) {
+            const PetscInt *nodes;
+            const PetscInt *norient;
+            DMPlexGetCone(dm, edges[i], &nodes);
+            DMPlexGetConeOrientation(dm, edges[i], &norient);
+            printf("%d %d %d: %d - %d\n", c, edges[i], orient[i], nodes[0], nodes[1]);
+        }
+    }
+    return 0;
 }
