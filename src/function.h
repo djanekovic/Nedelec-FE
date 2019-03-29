3db214057d7285397cf0dc2bf49fcece55b5b80b
Darko Janekovic <darko.janekovic@fer.hr>
2019-03-29 23:32:15 +0100

    function.h: added function interface
    Signed-off-by: Darko Janekovic <darko.janekovic@fer.hr>

--- src/function.h
+++ src/function.h
@@ -4,4 +4,9 @@
 PetscScalar constant_2D(PetscReal, PetscReal);
 PetscScalar constant_3D(PetscReal, PetscReal, PetscReal);
 
+struct function {
+    PetscScalar (*value_at_2D)(PetscReal, PetscReal);
+    PetscScalar (*value_at_3D)(PetscReal, PetscReal, PetscReal);
+};
+
 #endif /* FUNCTION_H */
