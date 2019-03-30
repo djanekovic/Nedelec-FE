fae7eca5616b00f30ad72285423d3cf804b1b9a1
Darko Janekovic <darko.janekovic@fer.hr>
2019-03-30 01:09:51 +0100

    nedelec.h: make offset static in loops
    This is hopefuly faster because offsets have to be computed only once
    and not before each access in the loop.
    
    Signed-off-by: Darko Janekovic <darko.janekovic@fer.hr>

--- src/nedelec.c
+++ src/nedelec.c
@@ -12,21 +12,25 @@ PetscErrorCode nedelec_basis(struct quadrature q, struct function_space *fspace)
     PetscErrorCode ierr = PetscMalloc1(3 * M * 2, &fspace->val); CHKERRQ(ierr);
     ierr = PetscMalloc1(3 * M * 1, &fspace->cval); CHKERRQ(ierr);
 
-    //TODO: clever define for this ugly addressing
     for (PetscInt i = 0; i < M; i++) {
+        int _i3 = 3 * i;
+        int offset_0 = 0 * (M * 2) + i * 2;
+        int offset_1 = 1 * (M * 2) + i * 2;
+        int offset_2 = 2 * (M * 2) + i * 2;
+
         // zero basis function for i-th quadrature point (x, y)
-        fspace->val[0 * (M * 2) + i * 2 + 0] = -q.pw[i * 3 + 1];
-        fspace->val[0 * (M * 2) + i * 2 + 1] = q.pw[i * 3 + 0];
+        fspace->val[offset_0 + 0] = -q.pw[_3i + 1];
+        fspace->val[offset_0 + 1] = q.pw[_3i + 0];
 
-        fspace->val[1 * (M * 2) + i * 2 + 0] = -q.pw[i * 3 + 1];
-        fspace->val[1 * (M * 2) + i * 2 + 1] = q.pw[i * 3 + 0] - 1;
+        fspace->val[offset_1 + 0] = -q.pw[_3i + 1];
+        fspace->val[offset_1 + 1] = q.pw[_3i + 0] - 1;
 
-        fspace->val[2 * (M * 2) + i * 2 + 0] = 1 - q.pw[i * 3 + 1];
-        fspace->val[2 * (M * 2) + i * 2 + 1] = q.pw[i * 3 + 0];
+        fspace->val[offset_2 + 0] = 1 - q.pw[_3i + 1];
+        fspace->val[offset_2 + 1] = q.pw[_3i + 0];
 
-        fspace->cval[i * 3 + 0] = 2;
-        fspace->cval[i * 3 + 1] = 2;
-        fspace->cval[i * 3 + 2] = 2;
+        fspace->cval[_3i + 0] = 2;
+        fspace->cval[_3i + 1] = 2;
+        fspace->cval[_3i + 2] = 2;
     }
 
     fspace->nbasis = 3;
