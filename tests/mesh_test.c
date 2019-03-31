330b411a95cef978d577f167973afd9d2de11adf
Darko Janekovic <darko.janekovic@fer.hr>
2019-03-31 17:51:43 +0200

    tests/mesh_test: added signs computations tests
    Added memory leak free tests for computing signs as well as discrete
    gradient matrix. Tested only on one processor.
    
    Signed-off-by: Darko Janekovic <darko.janekovic@fer.hr>

--- tests/mesh_test.c
+++ tests/mesh_test.c
@@ -7,6 +7,51 @@
 
 #include "../src/mesh.h"
 
+#define EPSILON 0.0001
+#define SIGNS_TEST_DATA  1,  1, -1, \
+                         1, -1,  1, \
+                        -1,  1, -1, \
+                        -1,  1, -1, \
+                         1, -1,  1, \
+                        -1,  1, -1, \
+                         1, -1,  1, \
+                         1, -1, -1
+
+#define COLS_TEST_DATA  8,  9, \
+                        9, 11, \
+                        8, 11, \
+                       12, 14, \
+                       11, 14, \
+                       11, 12, \
+                        9, 12, \
+                       10, 12, \
+                       10, 13, \
+                       12, 13, \
+                        9, 10, \
+                       13, 15, \
+                       13, 16, \
+                       15, 16, \
+                       12, 15, \
+                       14, 15
+
+#define VALS_TEST_DATA -1.0,  1.0, \
+                       -1.0,  1.0, \
+                        1.0, -1.0, \
+                       -1.0,  1.0, \
+                        1.0, -1.0, \
+                       -1.0,  1.0, \
+                       -1.0,  1.0, \
+                        1.0, -1.0, \
+                       -1.0,  1.0, \
+                        1.0, -1.0, \
+                       -1.0,  1.0, \
+                        1.0, -1.0, \
+                       -1.0,  1.0, \
+                        1.0, -1.0, \
+                        1.0, -1.0, \
+                        1.0, -1.0,
+
+
 const char *help = "Unit tests for meshing utility";
 
 static int init_petsc(void **data);
@@ -72,18 +117,48 @@ malloc_failed:
 
 static void test_signs(void **data)
 {
+    int test_signs[] = {SIGNS_TEST_DATA};
     struct test_ctx *tctx = *data;
-    assert_non_null(tctx->sctx->signs);
+    struct ctx *sctx = tctx->sctx;
+    assert_non_null(sctx->signs);
+
+    for (int i = sctx->cstart; i < sctx->cend; i++) {
+        for (int j = 0; j < 3; j++) {
+            int offset = i * 3 + j;
+            assert_int_equal(test_signs[offset], sctx->signs[offset]);
+        }
+    }
 }
 
 static void test_discrete_gradient(void **data)
 {
+    PetscInt test_cols[] = {COLS_TEST_DATA};
+    PetscScalar test_vals[] = {VALS_TEST_DATA};
     struct test_ctx *tctx = *data;
+    struct ctx *sctx = tctx->sctx;
+
     assert_non_null(tctx->sctx->G);
+
+    for (int i = 0; i < sctx->eend - sctx->estart; i++) {
+        PetscInt ncols;
+        const PetscInt *cols;
+        const PetscScalar *vals;
+        MatGetRow(sctx->G, i, &ncols, &cols, &vals);
+        for (int j = 0; j < ncols; j++) {
+            int offset = i * ncols + j;
+            assert_int_equal(test_cols[offset] - sctx->vstart, cols[j]);
+            assert_true(PetscAbsReal(test_vals[offset] - vals[j]) < EPSILON);
+        }
+        MatRestoreRow(sctx->G, i, &ncols, &cols, &vals);
+    }
 }
 
 static int finalize_petsc(void **data)
 {
+    struct test_ctx *tctx = *data;
+    DM *dm = tctx->dm;
+
+    DMDestroy(dm);
     PetscFinalize();
 
     return 0;
